37a7bd1ed832aa8409297ff0bb5bfaef82badc02
Yaroslav <yaroslav@wannabe.pro>
2019-11-19 18:01:40 +0500

    Fix actions

--- test_sample.py
+++ test_sample.py
@@ -2,12 +2,12 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class PythonOrgSearch(unittest.TestCase):
+class TestSample(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome()
 
-    def test_search_in_python_org(self):
+    def test_sample(self):
         driver = self.driver
         driver.get("http://localhost")
         self.assertIn("Welcome to nginx!", driver.title)
