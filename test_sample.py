a533a76ac1b375135e04bc36f6ee79325cc365a7
Yaroslav <yaroslav@wannabe.pro>
2019-11-20 19:54:34 +0500

    Fix yes

--- test_sample.py
+++ test_sample.py
@@ -1,11 +1,11 @@
 import unittest
-from selenium import webdriver
+from selenium.remote.webdriver import WebDriver
 from selenium.webdriver.common.keys import Keys
 
 class TestSample(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Chrome()
+        self.driver = WebDriver('http://localhost:4444/wd/hub', 'chrome', 'ANY')
 
     def test_sample(self):
         driver = self.driver
