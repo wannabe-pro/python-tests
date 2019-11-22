045d203d00ca6fd702902ce4331c89938fed7468
Yaroslav <yaroslav@wannabe.pro>
2019-11-22 13:05:00 +0500

    Add doc

---
+++ README.md
@@ -0,0 +1,7 @@
+# Пример тестов для Python с использованием Selenium
+
+![](https://github.com/wannabe-pro/python-tests/workflows/Python%20application/badge.svg)
+
+Данный пример по Push запускает Selenium и Nginx в Docker контейнере, и тестирует отображение заглушки. 
+
+Лицензия - MIT.
--- test_sample.py
+++ test_sample.py
@@ -2,21 +2,39 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
+"""
+Пример теста.
+Вcе тесты запускаются из файлов вида test_*.py или *_test.py.
+Перед выполнение тестов надо поднять контейнеры с сервисами selenium и nginx.
+"""
+
+
 class TestSample(unittest.TestCase):
 
     def setUp(self):
+        """
+        Инициализация теста, делаем соединение с selenium.
+        """
         self.driver = webdriver.Remote(
-            command_executor = 'http://127.0.0.1:4444/wd/hub',
-            desired_capabilities = DesiredCapabilities.CHROME
+            command_executor='http://127.0.0.1:4444/wd/hub',
+            desired_capabilities=DesiredCapabilities.CHROME
         )
 
     def test_sample(self):
+        """
+        Простой тест, переходим на страницу приметствия nginx и проверяем заголовок.
+        """
         driver = self.driver
-        driver.get("http://nginx")
-        self.assertIn("Welcome to nginx!", driver.title)
+        driver.get('http://nginx')
+        self.assertIn('Welcome to nginx!', driver.title)
 
     def tearDown(self):
+        """
+        Деструктор теста, убиваем соединение с selenium.
+        """
         self.driver.close()
 
-if __name__ == "__main__":
+
+# для совместимости с unittest
+if __name__ == '__main__':
     unittest.main()
